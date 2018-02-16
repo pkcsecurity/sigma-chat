9087b88c6630c3ec5474ec0e66b8a458391d582a
Arte Ebrahimi <arteebrahimi@gmail.com>
2018-02-16 15:01:35 -0800

    byte-arr to byte-buf

--- src/sigma_chat/common/utils.clj
+++ src/sigma_chat/common/utils.clj
@@ -67,5 +67,8 @@
 (defn byte-buf->hex-string [b-arr]
   (clojure.string/join (map #(.toUpperCase (format "%02x" %)) (.array b-arr))))
 
-(defn string->byte-arr [string]
+(defn hex-string->byte-arr [string]
   (DatatypeConverter/parseHexBinary string))
+
+(defn byte-arr->byte-buf [b-arr]
+  (ByteBuffer/wrap b-arr))
